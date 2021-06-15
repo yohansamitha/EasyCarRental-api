566459d1f91010bf37598bdc29358e8f4b5d0236
YohanSamitha <yohansamitha123@gmail.com>
2021-06-15 14:44:00 +0530

    add basic configaration

---
+++ src/main/java/lk/easycarrental/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package lk.easycarrental.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    @PostMapping
+    public String saveCustomer() {
+        return "hi";
+    }
+}
