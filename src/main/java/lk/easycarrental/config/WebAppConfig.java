2da7acd38f1b633f5b8eaa9e68f2e660c09d8dab
YohanSamitha <yohansamitha123@gmail.com>
2021-06-17 01:15:59 +0530

    add model mapper to Spring container

--- src/main/java/lk/easycarrental/config/WebAppConfig.java
+++ src/main/java/lk/easycarrental/config/WebAppConfig.java
@@ -1,5 +1,7 @@
 package lk.easycarrental.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,4 +10,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = "lk.easycarrental")
 public class WebAppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
