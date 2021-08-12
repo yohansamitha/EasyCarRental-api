9980b2045e64d84a1fbb6fea234152494713d0ee
yohansamitha <65862807+yohansamitha@users.noreply.github.com>
2021-08-12 09:58:48 +0530

    update

--- src/main/java/lk/easycarrental/util/StandardResponse.java
+++ src/main/java/lk/easycarrental/util/StandardResponse.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * @author : Sanu Vithanage
- * @since : 0.1.0
- **/
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
